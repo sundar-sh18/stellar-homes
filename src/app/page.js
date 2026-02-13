62f714928cbb442dbfda376b1a8a28025ce90b93
Sundar S <sundar.sh18@gmail.com>
2026-02-13 16:15:58 +0530

    components integration

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import Hero from './components/Hero/Hero'
 import Info from './components/Info/Info'
+import HeadNav from './components/Navbar/HeadNav'
+import Info2 from './components/Info/Info2'
 
 export default function Page() {
   return (
     <div className="">
       
-      
+      <HeadNav/>
       <div className="h-dvh bg-[url('/bg.jpg')] bg-cover bg-center bg-no-repeat">
         <Hero/>
       </div>
       <Info/>
+      <Info2/>
       
     </div>
   )

2321f0d61baf2a204978dbadf5fba790e92b99ae
Sundar S <sundar.sh18@gmail.com>
2026-02-13 16:18:10 +0530

    nav bar update

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
-// import React, { useState } from 'react';
-import { MoveRight } from 'lucide-react';
+
 import Link from 'next/link';
 import Image from 'next/image';
 import style from './NavBar.module.css'
+import './NavBar.module.css'
 
 export default function Navbar() {
 //   const [isOpen, setIsOpen] = useState(false);
@@ -32,14 +32,14 @@ export default function Navbar() {
 
   return (
     
-    <nav className={`${style} flex justify-between p-10  align-center items-center font-semibold`}>
-        <Link href="/home"><Image width={100} height={100} src="./next.svg" alt=""></Image></Link>
+    <nav className={`${style.trans} flex justify-between p-10  align-center items-center font-semibold transition-all duration-1000 ease-[cubic-bezier(0.075, 0.82, 0.165, 1)] `}>
+        <Link href="/home"><Image width={100} height={100} src="/favicon.ico" alt=""></Image></Link>
         <ul className='flex justify-between gap-10'>
             {nav.map((nav) => (
-                <Link className='hover:text-white' key={nav.id} href={nav.url}>{nav.name}</Link>
+                <Link className={`${style.line} ${style.trans} text-primary relative`} key={nav.id} href={nav.url}>{nav.name}</Link>
             ))}
         </ul>
-        <Link className='flex gap-2 hover:text-white' href="/Contact"><span><MoveRight/></span>Contact Us</Link>
+        <Link className={`${style.trans} flex gap-2 text-bg-1 hover:text-white line`} href="/Contact"><button className={`${style.button} ${style.type1} flex`}><span className={`${style.trans} ${style.btnTxt}`}></span>Contact Us</button></Link>
     </nav>
   );
 }

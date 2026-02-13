becdc6b32da26fe47ad1fb55086a43e4f861ce2c
Sundar S <sundar.sh18@gmail.com>
2026-02-13 16:18:25 +0530

    config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,15 +2,15 @@ const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
-  theme: {
-    extend: {
-      colors: {
-        primary: "#15342D",
-        secondary: "#DAE1DE",
-        accent: "#f59e0b",
-      },
-    },
-  },
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       primary: "#15342D",
+  //       secondary: "#DAE1DE",
+  //       accent: "#f59e0b",
+  //     },
+  //   },
+  // },
 };
 
 export default config;
